ced4c600e83509cce56c96634ff08706188432d7
Giacomo Petrillo <info@giacomopetrillo.com>
2020-04-28 13:49:13 +0200

    automatic version and copyright

--- docs/conf.py
+++ docs/conf.py
@@ -9,20 +9,29 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'lsqfitgp'
-copyright = '2020, Giacomo Petrillo'
 author = 'Giacomo Petrillo'
 
+from datetime import datetime
+now = datetime.now()
+year = '2020'
+if now.year > int(year):
+    year += '-' + str(now.year)
+copyright = year + ', ' + author
+
+
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+import lsqfitgp
+release = lsqfitgp.__version__
+version = release
 
 
 # -- General configuration ---------------------------------------------------
