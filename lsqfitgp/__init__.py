b1fe96cbc69f48b5713bef2e20392780c1c479d9
Giacomo Petrillo <info@giacomopetrillo.com>
2020-05-03 21:01:28 +0200

    nump version to 0.1.2

--- lsqfitgp/__init__.py
+++ lsqfitgp/__init__.py
@@ -6,15 +6,19 @@ from ._array import *
 from ._fit import *
 from ._Deriv import *
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 __doc__ = """
 
-Module to fit gaussian processes with gvar/lsqfit. It can both be used
-standalone to fit data with a gaussian process only, and with lsqfit inside a
-possibly nonlinear model with other parameters. In lsqfit style, all the
-results will be properly correlated with prior, data, and other non-gaussian
-process parameters in the fit, even when doing conditional prediction.
+Module to fit gaussian processes with gvar/lsqfit.
+
+Manual: https://lsqfitgp.readthedocs.io/en/latest
+
+It can both be used standalone to fit data with a gaussian process only, and
+with lsqfit inside a possibly nonlinear model with other parameters. In lsqfit
+style, all the results will be properly correlated with prior, data, and other
+non-gaussian process parameters in the fit, even when doing conditional
+prediction.
 
 The main class is `GP`, which represents a gaussian process over arbitrary
 input. It can be used both autonomously and with lsqfit. The inputs/outputs can
@@ -28,16 +32,14 @@ Functions and classes
     
     GP : class
         Class of objects representing a gaussian process.
-    kernel : decorator
-        Decorator for marking a function as a kernel.
-    isotropickernel : decorator
-        Decorator for marking a function as an isotropic kernel.
     empbayes_fit : function
-        Fit the hyperpriors of a gaussian process.
+        Fit the hyperparameters of a gaussian process.
     StructuredArray : class
         Autograd-friendly wrapper of numpy structured arrays.
     where : function
         Make a kernel that switches between two kernels based on a condition.
+    Deriv : class
+        Class representing a derivative specification.
 
 Kernels
 -------
@@ -48,50 +50,51 @@ distance between the arguments. Kernel objects can be summed, multiplied and
 raised to a power.
 
 To make a custom kernel, you can instantiate one of the two general classes by
-passing them a function, or subclass them. For convenience, decorators are
-provided to convert a function to a covariance kernel. Otherwise, use one of
-the already available subclasses listed below. Isotropic kernels are normalized
-to have unit variance and roughly unit lengthscale.
+passing them a function, or subclass them. For convenience, decorators `kernel`
+and `isotropickernel` are provided to convert a function to a covariance
+kernel. Otherwise, use one of the already available subclasses listed below.
+Isotropic kernels are normalized to have unit variance and roughly unit
+lengthscale.
 
-    Constant :
+    Constant
         Equivalent to fitting with a constant.
-    Linear :
+    Linear
         Equivalent to fitting with a line.
-    Polynomial :
+    Polynomial
         Equivalent to fitting with a polynomial.
-    ExpQuad :
+    ExpQuad
         Gaussian kernel.
-    White :
+    White
         White noise, each point is indipendent.
-    Matern :
+    Matern
         Matérn kernel, you can set how many times it is differentiable.
-    Matern12, Matern32, Matern52 :
+    Matern12, Matern32, Matern52
         Matérn kernel for the specific cases nu = 1/2, 3/2, 5/2.
-    GammaExp :
+    GammaExp
         Gamma exponential. Not differentiable, but you can set how close it is
         to being differentiable.
-    RatQuad :
+    RatQuad
         Equivalent to a mixture of gaussian kernels with gamma-distributed
         length scales.
-    NNKernel :
+    NNKernel
         Equivalent to training a neural network with one latent infinite layer.
-    Wiener :
+    Wiener
         Random walk.
-    WienerIntegral :
+    WienerIntegral
         Integral of the random walk.
-    Gibbs :
+    Gibbs
         A gaussian kernel with a custom variable length scale.
-    Periodic :
+    Periodic
         A periodic gaussian kernel, represents a periodic function.
-    Categorical :
+    Categorical
         Arbitrary covariance matrix over a finite set of values.
-    Cos :
+    Cos
         A cosine.
-    FracBrownian :
+    FracBrownian
         Fractional Brownian motion, like Wiener but with correlations.
-    PPKernel :
+    PPKernel
         Finite support isotropic kernel.
-    Rescaling :
+    Rescaling
         Kernel used to change the variance of other kernels.
 
 Reference: Rasmussen et al. (2006), "Gaussian Processes for Machine Learning".
