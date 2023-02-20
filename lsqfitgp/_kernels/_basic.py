fit: new parameters covariance and fix

lsqfitgp/_kernels/_basic.py
@@ -380,28 +380,24 @@ def CausalExpQuad(r, alpha=1):
     # TODO taylor-expand erfc near 0 and use r2
 
 @kernel(derivable=True, maxdim=1)
-def Decaying(x, y):
+def Decaying(x, y, alpha=1):
     """
     Decaying kernel.
     
     .. math::
         k(x, y) =
-        \\frac{1}{1 + x + y},
-        \\quad x, y \\ge 0
-    
-    It is infinitely divisible.
-    
+        \\frac{1}{(1 + x + y)^\\alpha},
+        \\quad x, y, \\alpha \\ge 0
+        
     Reference: Swersky, Snoek and Adams (2014).
     """
     # TODO high dimensional version of this, see mlkernels issue #3
     with _patch_jax.skipifabstract():
         assert jnp.all(x >= 0)
         assert jnp.all(y >= 0)
-    return 1 / (x + y + 1)
+    return 1 / (x + y + 1) ** alpha
     # use x + y + 1 instead of 1 + x + y because the latter is less numerically
-    # symmetric for small x and y
-    
-    # TODO infinite divisibility checking system like maxdim and derivable
+    # accurate and symmetric for small x and y
 
 @isotropickernel(derivable=False, input='soft')
 def Log(r):

tests/test_kernels.py
@@ -767,7 +767,10 @@ test_kwargs = {
     _kernels.Bessel: dict(kwargs_list=[dict()] + [dict(nu=nu) for nu in range(5)] + [dict(nu=nu - 0.01) for nu in range(1, 5)] + [dict(nu=nu + 0.01) for nu in range(5)] + [dict(nu=nu + 0.5) for nu in range(5)]),
     _kernels.Cauchy: dict(kwargs_list=[dict(alpha=a, beta=b) for a in [0.001, 0.5, 0.999, 1, 1.001, 1.5, 1.999, 2] for b in [0.001, 0.5, 1, 1.5, 2, 4, 8]]),
     _kernels.CausalExpQuad: dict(kwargs_list=[dict(alpha=a) for a in [0, 1, 2]]),
-    _kernels.Decaying: dict(random_x_fun=lambda **_: np.random.uniform(0, 5, size=100)),
+    _kernels.Decaying: dict(
+        random_x_fun=lambda **_: np.random.uniform(0, 5, size=100)
+        kwargs_list=[dict(alpha=a) for a in [0, .5, 1, 2]],
+    ),
     _kernels.StationaryFracBrownian: dict(kwargs_list=[dict(H=H) for H in [0.1, 0.5, 1]]),
     _kernels.Circular: dict(kwargs_list=[dict(c=c, tau=t) for c, t in [(0.1, 4), (0.5, 4), (0.5, 8)]]),
     _kernels.MA: dict(