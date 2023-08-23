e038b386ee5414c5d1f6d7590261259d8e9aa479
Giacomo Petrillo <info@giacomopetrillo.com>
2023-08-23 11:30:06 +0200

    maint: cleanup unused code

--- src/lsqfitgp/_patch_jax/__init__.py
+++ src/lsqfitgp/_patch_jax/__init__.py
@@ -142,55 +142,6 @@ class skipifabstract:
             # in examples/pdf4.py
             return True
 
-# TODO make stop_hessian work in reverse mode
-# see jax issue #10994
-# from jax.interpreters import ad
-# from jax._src import ad_util
-# ad.primitive_transposes[ad_util.stop_gradient_p] = lambda ct, _: [lax.stop_gradient(ct)]
-# I tried with jax.jvp, lax.stop_gradient, and combinations of anything, but I
-# failed
-
-# @jax.custom_jvp
-# def stop_hessian(x):
-#     return x
-
-# @stop_hessian.defjvp
-# def stop_hessian_jvp(primals, tangents):
-#     x, = primals
-#     x_dot, = tangents
-#     return x, lax.stop_gradient(x_dot)
-
-# def value_and_ops(f, *ops, has_aux=False, **kw):
-#     """
-#     Creates a function returning the values of f and its derivatives defined
-#     by stacking the operators in ops. Example:
-#     value_and_grad_and_hess = value_and_ops(f, jax.grad, jax.jacfwd)
-#     """
-#     if not ops:
-#         return f
-#     def fop(*args, **kw):
-#         y = f(*args, **kw)
-#         if has_aux:
-#             y, aux = y
-#             return y, (aux,)
-#         else:
-#             return y, ()
-#     def nextfop(fop):
-#         def nextfop(*args, **kw):
-#             y, aux = fop(*args, **kw)
-#             return y, aux + (y,)
-#         return nextfop
-#     for op in ops:
-#         fop = op(nextfop(fop), has_aux=True, **kw)
-#     @functools.wraps(f)
-#     def lastfop(*args, **kw):
-#         y, aux = fop(*args, **kw)
-#         if has_aux:
-#             return aux[1:] + (y,), aux[0]
-#         else:
-#             return aux + (y,)
-#     return lastfop
-
 def float_type(*args):
     t = jnp.result_type(*args)
     return jnp.sin(jnp.empty(0, t)).dtype
