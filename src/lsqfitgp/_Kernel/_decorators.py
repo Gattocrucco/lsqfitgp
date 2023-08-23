8e72e1558c1f65bc96c89546b7855c267a7adb73
Giacomo Petrillo <info@giacomopetrillo.com>
2023-08-24 00:47:41 +0200

    kernel: improve decorators docstrings

--- src/lsqfitgp/_Kernel/_decorators.py
+++ src/lsqfitgp/_Kernel/_decorators.py
@@ -33,31 +33,26 @@ def _makekernelsubclass(kernel, superclass, **prekw):
         named_object = kernel
     
     name = getattr(named_object, '__name__', 'DecoratedKernel')
-    newclass = types.new_class(name, (superclass,))
 
-    # TODO use the exec_body parameter of new_class instead of doing everything
-    # after creation, since an eventual __init_subclass__ wouldn't see the
-    # definitions.
+    def exec_body(ns):
         
-    prekwset = set(prekw)
-    def __new__(cls, **kw):
-        kwargs = prekw.copy()
-        shared_keys = prekwset & set(kw)
-        if shared_keys:
-            msg = 'overriding init argument(s) ' + ', '.join(shared_keys)
-            msg += ' of kernel ' + name
-            warnings.warn(msg)
-        kwargs.update(kw)
-        return super(newclass, cls).__new__(cls, kernel, **kwargs)
-    
-    newclass.__new__ = __new__
-    newclass.__wrapped__ = named_object
-    newclass.__doc__ = named_object.__doc__
-    newclass.__qualname__ = getattr(named_object, '__qualname__', name)
-    
-    # TODO functools.wraps raised an error, but maybe functools.update_wrapper
-    # with appropriate arguments would work even on a class.
-    
+        prekwset = set(prekw)
+        def __new__(cls, **kw):
+            kwargs = prekw.copy()
+            shared_keys = prekwset & set(kw)
+            if shared_keys:
+                msg = 'overriding init argument(s) ' + ', '.join(shared_keys)
+                msg += ' of kernel ' + name
+                warnings.warn(msg)
+            kwargs.update(kw)
+            return super(newclass, cls).__new__(cls, kernel, **kwargs)
+        
+        ns['__new__'] = __new__
+        ns['__wrapped__'] = named_object
+        ns['__doc__'] = named_object.__doc__
+
+    newclass = types.new_class(name, (superclass,), exec_body=exec_body)
+
     return newclass
 
 def _kerneldecoratorimpl(cls, *args, **kw):
@@ -72,11 +67,27 @@ def _kerneldecoratorimpl(cls, *args, **kw):
 def kernel(*args, **kw):
     """
     
-    Decorator to convert a function to a subclass of `Kernel`. Example::
+    Decorator to convert a function to a subclass of `Kernel`.
+
+    Parameters
+    ----------
+    *args :
+        Either a function to decorate, or no arguments.
+    **kw :
+        Additional arguments are passed to `Kernel`.
+
+    Returns
+    -------
+    fun_or_dec : callable
+        If `args` is empty, a decorator ready to be applied, else the decorated
+        function.
+
+    Examples
+    --------
     
-        @kernel(loc=10) # the default loc will be 10
-        def MyKernel(x, y, cippa=1, lippa=42):
-            return cippa * (x * y) ** lippa
+    >>> @lgp.kernel(loc=10) # the default loc will be 10
+    ... def MyKernel(x, y, cippa=1, lippa=42):
+    ...     return cippa * (x * y) ** lippa
     
     """
     return _kerneldecoratorimpl(_kernel.Kernel, *args, **kw)
@@ -85,14 +96,29 @@ def stationarykernel(*args, **kw):
     """
     
     Decorator to convert a function to a subclass of `StationaryKernel`.
-    Example::
     
-        @stationarykernel(input='soft')
-        def MyKernel(absdelta, cippa=1, lippa=42):
-            return cippa * sum(
-                np.exp(-absdelta[name] / lippa)
-                for name in absdelta.dtype.names
-            )
+    Parameters
+    ----------
+    *args :
+        Either a function to decorate, or no arguments.
+    **kw :
+        Additional arguments are passed to `StationaryKernel`.
+
+    Returns
+    -------
+    fun_or_dec : callable
+        If `args` is empty, a decorator ready to be applied, else the decorated
+        function.
+
+    Examples
+    --------
+    
+    >>> @lgp.stationarykernel(input='soft')
+    ... def MyKernel(absdelta, cippa=1, lippa=42):
+    ...     return cippa * sum(
+    ...         jnp.exp(-absdelta[name] / lippa)
+    ...         for name in absdelta.dtype.names
+    ...     )
     
     """
     return _kerneldecoratorimpl(_stationary.StationaryKernel, *args, **kw)
@@ -101,11 +127,26 @@ def isotropickernel(*args, **kw):
     """
     
     Decorator to convert a function to a subclass of `IsotropicKernel`.
-    Example::
     
-        @isotropickernel(derivable=True)
-        def MyKernel(distsquared, cippa=1, lippa=42):
-            return cippa * np.exp(-distsquared) + lippa
+    Parameters
+    ----------
+    *args :
+        Either a function to decorate, or no arguments.
+    **kw :
+        Additional arguments are passed to `IsotropicKernel`.
+
+    Returns
+    -------
+    fun_or_dec : callable
+        If `args` is empty, a decorator ready to be applied, else the decorated
+        function.
+
+    Examples
+    --------
+    
+    >>> @lgp.isotropickernel(derivable=True)
+    ... def MyKernel(distsquared, cippa=1, lippa=42):
+    ...     return cippa * jnp.exp(-distsquared) + lippa
     
     """
     return _kerneldecoratorimpl(_isotropic.IsotropicKernel, *args, **kw)
